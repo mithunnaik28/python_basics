356ebc87583ebf85e35a987076849f631bbaaaea
MITHUN NAIK <mithunnaik28c@gmail.com>
2026-08-03 19:18:23 +0530

    Create Lisstring_to_list_using_split.py

---
+++ Lisstring_to_list_using_split.py
@@ -0,0 +1,14 @@
+s= "Bhoomika is kulli"
+s1= "Bhoomika-is-kulli"
+
+l=s.split()
+l1=s1.split()
+l2=s1.split("-")
+
+print(l)
+print(l1)
+print(l2)
+
+# ['Bhoomika', 'is', 'kulli']
+# ['Bhoomika-is-kulli']
+# ['Bhoomika', 'is', 'kulli']
