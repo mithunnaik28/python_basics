6e589e95e222b39e1bc87c759b4c028842632d8b
MITHUN NAIK <mithunnaik28c@gmail.com>
2026-08-04 17:34:53 +0530

    Create input_to_list.py

---
+++ List/input_to_list.py
@@ -0,0 +1,13 @@
+x=input("enter the numbers:").split()
+
+l=[int(num) for num in x]
+print(l)
+
+        # OR
+x1=[int(num) for num in input("enter the numbers:").split()]
+print(x1)
+
+# enter the numbers:12 34 
+# [12, 34]
+# enter the numbers:12 34 
+# [12, 34]
