38d5f42ac0c9b98830482d464faffbb270364e1f
MITHUN NAIK <mithunnaik28c@gmail.com>
2026-08-06 20:38:42 +0530

    Create sort_in_list.py

---
+++ List/sort_in_list.py
@@ -0,0 +1,13 @@
+l = [64,46,476,54,12,89,65,32,8]
+
+print(l)
+
+l.sort() # ascending order 
+print(l)
+
+l.sort(reverse = True) # descending order
+print(l)
+
+# [64, 46, 476, 54, 12, 89, 65, 32, 8]
+# [8, 12, 32, 46, 54, 64, 65, 89, 476]
+# [476, 89, 65, 64, 54, 46, 32, 12, 8]
