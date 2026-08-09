a525f5601db981d64700f0e0100a53374a77be70
MITHUN NAIK <mithunnaik28c@gmail.com>
2026-08-09 11:43:01 +0530

    Create tables.py

---
+++ Functions/tables.py
@@ -0,0 +1,18 @@
+def table(t):
+    for i in range(1,11):
+        print(f"{t}*{i}={t*i}")
+
+tb=int(input("which table :"))
+table(tb)
+
+# which table :5
+# 5*1=5
+# 5*2=10
+# 5*3=15
+# 5*4=20
+# 5*5=25
+# 5*6=30
+# 5*7=35
+# 5*8=40
+# 5*9=45
+# 5*10=50
