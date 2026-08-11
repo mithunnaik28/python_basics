9596972ff2c430b066ecc5b95641f0db9d21b328
MITHUN NAIK <mithunnaik28c@gmail.com>
2026-08-11 13:00:35 +0530

    Create nested_functions.py

---
+++ Functions/nested_functions.py
@@ -0,0 +1,17 @@
+def calculater(a,b):
+    def add():
+        print(a+b)
+    def sub():
+        print(a-b)
+    def mul():
+        print(a*b)
+
+    add()
+    sub()
+    mul()
+
+calculater(10,5)
+
+# 15
+# 5
+# 50
